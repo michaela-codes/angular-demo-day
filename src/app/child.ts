86718db5c6f6fa34c279de7a614b99a771b99147
michaela <michaela.childers216@gmail.com>
2022-11-28 17:08:40 -0600

    .

--- src/app/child.ts
+++ src/app/child.ts
@@ -3,7 +3,7 @@ import { Time } from "@angular/common";
 export interface Child {
     name: string;
     age: number;
-    allergies: string;
+    allergies?: string;
     bedtime: Time;
-    favFood: string
+    favFood?: string
 }
